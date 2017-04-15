68c3ad7c290b7749950fe649a57cafaaae8bb543
Demon7666 <dimon-7666@mail.ru>
2017-04-15 17:39:44 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<h1> Информация о себе </h1>
+<?php
+error_reporting(E_ALL);
+$name = 'Дмитрий';
+$age = '24';
+$mail = '<a href="dimon-7666@mail.ru"> dimon-7666@mail.ru </a>';
+$city = 'Москва';
+$about = 'Ведущий специалист';
+echo ' Имя:  ', $name; 
+echo '</br>';
+echo ' Возраст:  ', $age;
+echo '</br>';
+echo ' Електронная почта:  ', $mail;
+echo '</br>';
+echo ' Город:  ', $city;
+echo '</br>';
+echo ' О себе:  ', $about;
+?>
+
